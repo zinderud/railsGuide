76f93761a78564b695988211e497308e9599b073
zinderud <mokosam@gmail.com>
2017-10-10 16:27:08 +0300

    devise

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :idea
+  has_many :users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   root to: redirect('/ideas')
   resources :ideas
