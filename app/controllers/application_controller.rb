cf85b95fed98553e2026044cd99d8b222efe718e
zinderud <mokosam@gmail.com>
2017-10-12 05:22:39 +0300

    r

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def merhaba
+    #render plain: 'Merhaba!'
+    #render({plain:'merhaba'})
+   # render 'application/merhaba'
+    #render inline: File.read('app/views/application/merhaba.html')
+    name=params['name']
+    render 'application/merhaba',locals:{
+      name:name
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
+#  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   resources :comments
   root to: redirect('/ideas')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/merhaba'=>'application#merhaba'
+  get '/merhaba/:name=>application#merhaba'
 end
