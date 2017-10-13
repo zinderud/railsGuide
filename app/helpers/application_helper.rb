6d3aace2aaf645131bff02c8a92425afd26a979c
zinderud <mokosam@gmail.com>
2017-10-13 17:10:24 +0300

    helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  module ApplicationHelper
+    def my_link_to(text, href)
+      "<a href='#{href}'>#{text}</a>".html_safe
+    end
+  end
+  #usage <%= my_link_to 'Back to Posts', posts_path %>
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
   mount_uploader :picture, PictureUploader
   has_many :comments
+  validates_presence_of :description
+  validates :name,presence: true,uniqueness: false
 end
